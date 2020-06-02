a896622e03ec6846581e6b153b7de42dfe447f69
chenyf <1424407762@qq.com>
2020-06-02 20:45:57 +0800

    完成项目初始化

---
+++ src/main/java/com/cyf/mystorebackstage/MyStoreBackstageApplication.java
@@ -0,0 +1,13 @@
+package com.cyf.mystorebackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStoreBackstageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyf/mystorebackstage/MyStoreBackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyf.mystorebackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreBackstageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
